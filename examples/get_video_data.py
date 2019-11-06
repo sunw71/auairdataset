00daa0a117265721fcf72ab3ba700c30b89d9115
bozcani <ilkerbozcan@hotmail.com>
2019-11-06 15:08:18 +0100

    Create video seq example

---
+++ examples/get_video_data.py
@@ -0,0 +1,15 @@
+from auairtools.auair import AUAIR
+
+# Paths to annotation file and source data.
+annotFile = '/home/ilker/Desktop/baum/workspace/DATASET/annotations.json'
+dataDir = '/home/ilker/Desktop/baum/workspace/DATASET/images'
+
+# Create a AUAIR object.
+auairdataset = AUAIR(annotation_file=annotFile, data_folder = dataDir)
+
+##############################################################
+######################### TODO  ##############################
+##############################################################
+
+##############################################################
+
