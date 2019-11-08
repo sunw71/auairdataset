1ac421d9c619a006f033c8fdfa122ad80888dd1a
bozcani <ilkerbozcan@hotmail.com>
2019-11-08 13:09:00 +0100

    Fix __le__()

--- auairtools/utils.py
+++ auairtools/utils.py
@@ -79,37 +79,37 @@ class TimeStamp:
     def __le__(self, otherTimeStamp):
         if self.year <= otherTimeStamp.year:
             return True
-        elif self.year <= otherTimeStamp.year:
+        elif self.year > otherTimeStamp.year:
             return False
         else:
             if self.month <= otherTimeStamp.month:
                 return True
-            elif self.month <= otherTimeStamp.month:
+            elif self.month > otherTimeStamp.month:
                 return False
             else:
                 if self.day <= otherTimeStamp.day:
                     return True
-                elif self.day <= otherTimeStamp.day:
+                elif self.day > otherTimeStamp.day:
                     return False
                 else:
                     if self.hour <= otherTimeStamp.hour:
                         return True
-                    elif self.hour <= otherTimeStamp.hour:
+                    elif self.hour > otherTimeStamp.hour:
                         return False
                     else:
                         if self.mins <= otherTimeStamp.mins:
                             return True
-                        elif self.mins <= otherTimeStamp.mins:
+                        elif self.mins > otherTimeStamp.mins:
                             return False
                         else:  
                             if self.sec <= otherTimeStamp.sec:
                                 return True
-                            elif self.sec <= otherTimeStamp.sec:
+                            elif self.sec > otherTimeStamp.sec:
                                 return False
                             else:  
                                 if self.ms <= otherTimeStamp.ms:
                                     return True
-                                elif self.ms <= otherTimeStamp.ms:
+                                elif self.ms > otherTimeStamp.ms:
                                     return False
                                 else: return False                                                                                                                        
 
